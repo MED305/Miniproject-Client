acc9116ee43accb513e5a8dce32307b8b5eac527
Hector <hectorthoegersen@gmail.com>
2020-09-17 15:37:49 +0200

    Added animation and sprite classes

--- src/main/java/entity/Actor.java
+++ src/main/java/entity/Actor.java
@@ -1,5 +1,24 @@
 package entity;
 
-public class Actor {
+public abstract class Actor {
 
+    private final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
+
+    protected int currentAnimation;
+
+    protected Animation actorAnimation;
+    protected Sprite actorSprite;
+    protected Vector2f actorPosition;
+
+    protected boolean movingUp, movingDown, movingLeft, movingRight, isAttacking;
+    protected int attackSpeed;
+
+    protected float directionX, directionY;
+
+    protected float maxSpeed, acceleration, deacceleration;
+
+    public Actor(Sprite sprite, Vector2f origin) {
+        this.actorSprite = sprite;
+        this.actorPosition = origin;
+    }
 }
