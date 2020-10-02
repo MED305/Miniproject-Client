dfc045e1b32b69f530d5c0a9c0449c39c8bf3336
Hector <hectorthoegersen@gmail.com>
2020-10-02 09:25:40 +0200

    Added input package

---
+++ src/main/java/input/PlayerController.java
@@ -0,0 +1,25 @@
+package input;
+
+import java.awt.event.*;
+
+public class PlayerController implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
