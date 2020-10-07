a3b9b09ebaeffe1ab0a379c4068f5fa8a8089cdc
Hector <hectorthoegersen@gmail.com>
2020-10-07 12:38:24 +0200

    Fixed sprite

--- src/main/java/entity/Actor.java
+++ src/main/java/entity/Actor.java
@@ -1,5 +1,6 @@
 package entity;
 
+import graphics.Sprite;
 import math.Vector2f;
 
 public abstract class Actor {
@@ -11,11 +12,11 @@ public abstract class Actor {
     protected int attackSpeed;
 
     protected Vector2f position, direction;
+    protected Sprite sprite;
 
     protected float maxSpeed, acceleration, deacceleration;
 
     public Actor() {
-        position = new Vector2f(0, 0);
-
+        position = new Vector2f(30, 30);
     }
 }
