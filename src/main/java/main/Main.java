482661f058efd65775d18e57a30cc9b008c6c5d5
Kresten <kresten.bild.carr@gmail.com>
2020-10-16 10:55:43 +0200

    starting a client socket

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,9 @@ package main;
 import graphics.GameLauncher;
 
 public class Main {
-
+    ClientSocket clientSocket = new Clientsocket("localhost", 6969);
+    clientSocket.connect();
+    
     public static void main(String[] args) {
 
         new GameLauncher();
