b97e501aff84bb39bf9109e52adf01dfe7a845f8
Kresten <kresten.bild.carr@gmail.com>
2020-10-16 11:37:21 +0200

    Update Main.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,11 +3,9 @@ package main;
 import graphics.GameLauncher;
 
 public class Main {
-    ClientSocket clientSocket = new Clientsocket("localhost", 6969);
-    clientSocket.connect();
-    
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        ClientSocket clientSocket = new ClientSocket("localhost", 6969);
+        clientSocket.connect();
         new GameLauncher();
 
     }
