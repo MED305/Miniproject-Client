00f65be049a499943695301d45ec58c2dba9ff4c
Hector <hectorthoegersen@gmail.com>
2020-10-16 10:12:20 +0200

    Fixed bugs

--- src/main/java/states/PlayState.java
+++ src/main/java/states/PlayState.java
@@ -3,6 +3,7 @@ package states;
 import graphics.Sprite;
 import input.KeyHandler;
 import input.MouseHandler;
+import graphics.Font;
 import math.Vector2f;
 
 import java.awt.*;
@@ -10,20 +11,20 @@ import java.awt.*;
 public class PlayState extends GameState {
     private Font font;
 
-    public PlayState(GameStateManager gsm){
+    public PlayState(GameStateManager gsm) {
         super(gsm);
-        font = new Font("data/Font/ZeldaFont.png", 16,16);
+        font = new Font("data/Font/ZeldaFont.png", 16, 16);
     }
 
     public void update() {
 
     }
 
-    public void input(MouseHandler mouse, KeyHandler key){
+    public void input(MouseHandler mouse, KeyHandler key) {
 
     }
 
-    public void render(Graphics2D g){
-        Sprite.drawArray(g, font, "HELLO WORLD", new Vector2f( 100,100), 32, 32, 16, 16;
+    public void render(Graphics2D g) {
+        Sprite.drawArray(g, font, "HELLO WORLD", new Vector2f(100, 100), 32, 32, 16, 16);
     }
 }
