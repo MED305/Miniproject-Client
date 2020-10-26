2f72b12416712f3d34786429afd5cf9c2c84a3a7
Hector <hectorthoegersen@gmail.com>
2020-10-26 21:15:16 +0100

    Cleanup

--- core/src/main/java/entity/Bullet.java
+++ core/src/main/java/entity/Bullet.java
@@ -1,7 +1,6 @@
 package main.java.entity;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import main.java.Main;
