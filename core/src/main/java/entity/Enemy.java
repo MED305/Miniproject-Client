4cbee98b41cb5d45e1f1127007ba8691329d38ae
Hector <hectorthoegersen@gmail.com>
2020-10-26 21:54:01 +0100

    Added Readme

--- core/src/main/java/entity/Enemy.java
+++ core/src/main/java/entity/Enemy.java
@@ -7,7 +7,6 @@ public class Enemy extends Entity {
     TextureAtlas.AtlasRegion sprite;
     PlayerActor target;
 
-
     public Enemy(TextureAtlas.AtlasRegion c_sprite, SpriteBatch c_batch, TextureAtlas c_atlas, PlayerActor c_target) {
         super(c_batch, c_atlas);
         sprite = c_sprite;
