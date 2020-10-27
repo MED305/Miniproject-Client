b2b57915f3ecc0fba36d3af4ed7c5e6036bea43f
VengeanceU <54887602+VengeanceU@users.noreply.github.com>
2020-10-27 12:39:15 +0100

    Update Bullet.java

--- core/src/main/java/entity/Bullet.java
+++ core/src/main/java/entity/Bullet.java
@@ -3,10 +3,14 @@ package main.java.entity;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
 import main.java.Main;
+
+import java.lang.annotation.Target;
 
 public class Bullet extends Entity {
 
+    private Vector2 movement;
     public static final int SPEED = 400;
     private static TextureAtlas.AtlasRegion texture;
     private final PlayerActor player;
@@ -16,18 +20,25 @@ public class Bullet extends Entity {
     public boolean remove = false;
 
     public Bullet(SpriteBatch c_batch, TextureAtlas c_atlas, PlayerActor c_player) {
+
         super(c_batch, c_atlas);
         player = c_player;
-        this.x = player.position.x;
-        this.y = player.position.y;
+        this.position = new Vector2(player.position.x, player.position.y);
 
         if (texture == null) {
             texture = atlas.findRegion("player/Bullet/Bullet1");
         }
+     movement = new Vector2(Gdx.input.getX(), Gdx.input.getY()).sub(this.position);
+        movement.nor().scl(SPEED);
+
     }
+
+
 
     public void update(float deltaTime) {
         batch.draw(texture, x, y);
+        this.movement.scl(deltaTime);
+        this.position.add(movement);
 
         y += SPEED * deltaTime;
         if (y > Gdx.graphics.getHeight()) {
